5bcf846a63da46460a8e396315be3ab69980ae11
thegentil <nicogentil@gmail.com>
2015-03-26 11:54:58 -0300

    Versão 14
    adicionando a funcao normalize

--- Projeto 2 - Forca.py
+++ Projeto 2 - Forca.py
@@ -14,8 +14,19 @@ import turtle               # Importando a biblioteca de turtle graphics
 window = turtle.Screen()               # criando uma janela
 window.bgcolor("lightblue")               # Definindo a cor de fundo para azul
 window.title("Jogo da Forca")               # Criando um título para o jogo
+turtle.speed(0)
 
 from random import choice               # Importando a função random
+
+from unicodedata import normalize               # Importando a função normalize
+
+# Comandos da função normalize, que irá retirar os acentos e maiusculas das palavras 
+
+def formatar(txt):
+    return normalize('NFKD', txt).encode('ASCII','ignore').decode('ASCII')
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()    
 
 # Selecionando o arquivo com a lista
 
@@ -36,13 +47,21 @@ for palavra in conteudo:              # Faz com que o comando do strip repita em
 
 # Criando um random para a lista
 
-esc = choice(limpa)
+esc = choice(limpa)              # Palavra que será randomizada
 
 print(len(esc))
 
 print(limpa)
 
 print(esc)
+
+# Criando a lista das palavras formatadas
+
+formatada = (formatar(esc)).lower()
+
+print(formatada)
+
+
   
          
 # Definindo a posição da forca
@@ -186,7 +205,7 @@ def espaco():
 # Definindo os espaços que estarão as letras
     
 turtle.penup()
-turtle.setx(-200)
+turtle.setx(-300)
 turtle.sety(-200)
 turtle.pendown()
 turtle.right(180)    
@@ -197,14 +216,15 @@ for c in esc:
     turtle.fd(10)
     turtle.pendown()
     
-    if c == '':
+    if c == ' ':
         espaco()    
 
 # Criando a função while para repetir o programa
 
-x = 0
+acertos = 0
+erros = 0
 
-while x == 0:
+while erros != 8:
     
     #if 
     
@@ -212,9 +232,9 @@ while x == 0:
     
     p = window.textinput("Pergunta", "Insira uma letra:")
     
-    if p in limpa(0):
+    if p in esc:
         
-        turtle
+        turtle.write()
     
     
     
