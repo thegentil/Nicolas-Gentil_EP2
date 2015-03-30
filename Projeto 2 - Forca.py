583499c4220019b0f9e2d506ea43fe6518bbecc0
thegentil <nicogentil@gmail.com>
2015-03-30 10:29:40 -0300

    Versão 18

--- Projeto 2 - Forca.py
+++ Projeto 2 - Forca.py
@@ -10,7 +10,26 @@ Projeto 2 - Jogo da Forca!
 
 # Projeto de Nicolas Gentil
 
-restart = 0
+from random import choice               # Importando a função random
+    
+from unicodedata import normalize               # Importando a função normalize
+
+arquivo = open('entrada.txt', encoding="utf-8")               # Selecionando o arquivo com a lista
+
+conteudo = arquivo.readlines()               # cria uma lista do arquivo
+
+
+# Limpando a lista de palavras
+
+limpa = []               # Criando uma lista de palavras limpas
+
+for palavra in conteudo:              # Faz com que o comando do strip repita em cada palavra
+    p = palavra.strip()               
+    if p != "":               # Elimina a linha em branco
+        limpa.append(p)               # Adiciona as palavras limpas para a nova lista
+    
+
+restart = 0               # Definindo restart
 
 while restart == 0:
 
@@ -18,24 +37,25 @@ while restart == 0:
     
     
     window = turtle.Screen()               # criando uma janela
-    window.bgcolor("lightblue")               # Definindo a cor de fundo para azul
     window.title("Jogo da Forca")               # Criando um título para o jogo
-    turtle.speed(0)
-    turtle.screensize(2000,800)
+    turtle.speed(0)               # Definindo a velocidade da tartaruga
+    turtle.screensize(2000,800)               # Definindo o tamanho da tela
     
-    turtle2 = turtle.Turtle()
-    turtle2.speed(0)
-    turtle2.pensize(8)
-    turtle2.hideturtle()
+    turtle2 = turtle.Turtle()               # criando uma nova tartaruga
+    turtle2.speed(0)               # Definindo a velocidade da tartaruga
+    turtle2.pensize(8)               # Definindo a grossura da caneta
+    turtle2.hideturtle()               # Escondendo a tartaruga
     
-    turtle3 = turtle.Turtle()
-    turtle3.pensize(8)
-    turtle3.speed(0)
-    turtle3.hideturtle()
+    turtle3 = turtle.Turtle()               # criando uma nova tartaruga
+    turtle3.pensize(8)               # Definindo a grossura da caneta
+    turtle3.speed(0)               # Definindo a velocidade da tartaruga
+    turtle3.hideturtle()               # Escondendo a tartaruga
     
-    from random import choice               # Importando a função random
+    turtle4 = turtle.Turtle()               # criando uma nova tartaruga
+    turtle4.pensize(8)               # Definindo a grossura da caneta
+    turtle4.speed(0)               # Definindo a velocidade da tartaruga
+    turtle4.hideturtle()               # Escondendo a tartaruga
     
-    from unicodedata import normalize               # Importando a função normalize
     
     # Comandos da função normalize, que irá retirar os acentos e maiusculas das palavras 
     
@@ -45,26 +65,12 @@ while restart == 0:
         from doctest import testmod
         testmod()    
     
-    # Selecionando o arquivo com a lista
-    
-    arquivo = open('entrada.txt', encoding="utf-8")
-    
-    conteudo = arquivo.readlines()               # cria uma lista do arquivo
-    
-    
-    # Limpando a lista de palavras
-    
-    limpa = []
-    
-    for palavra in conteudo:              # Faz com que o comando do strip repita em cada palavra
-        p = palavra.strip()               
-        if p != "":               # Elimina a linha em branco
-            limpa.append(p)               # Adiciona as palavras limpas para a nova lista
-    
     
     # Criando um random para a lista
     
     esc = choice(limpa)              # Palavra que será randomizada
+    
+    limpa.remove(esc)               # Remove a palavra já escolhida
     
     print(len(esc))
     
@@ -77,11 +83,9 @@ while restart == 0:
     formatada = (formatar(esc)).lower()              # Palavra formatada
     
     print(formatada)
-    
-    
-      
+     
              
-    # Definindo a posição da forca
+    # Criando a forca
              
     def forca():         
     
@@ -112,9 +116,6 @@ while restart == 0:
         turtle2.pendown()
         turtle2.fillcolor("yellow")
         turtle2.circle(30)
-         
-    
-    #cabeca()
     
     # Criando o corpo
     
@@ -124,9 +125,7 @@ while restart == 0:
         turtle2.left(90)
         turtle2.fd(60)
         turtle2.pendown()
-        turtle2.fd(70)
-        
-    #corpo()    
+        turtle2.fd(70)  
     
     # Criando a primeira perna
     
@@ -138,8 +137,6 @@ while restart == 0:
         turtle2.setx(-100)
         turtle2.sety(-30) 
     
-    #perna1()
-    
     # Criando a segunda perna
     
     def perna2():
@@ -147,9 +144,7 @@ while restart == 0:
         turtle2.pendown()
         turtle2.left(60)
         turtle2.fd(50)
-    
-    #perna2()    
-    
+
     # Criando o primeiro braço
     
     def braco1():
@@ -160,9 +155,7 @@ while restart == 0:
         turtle2.left(60)
         turtle2.pendown()
         turtle2.fd(30)
-        
-    #braco1()    
-    
+
     # Criando o segundo braço
     
     def braco2():
@@ -171,9 +164,7 @@ while restart == 0:
         turtle2.right(180)
         turtle2.pendown()
         turtle2.fd(60)
-        
-    #braco2()    
-    
+
     # Criando os olhos
     
     def olhos():
@@ -193,8 +184,6 @@ while restart == 0:
         turtle2.right(180)
         turtle2.pendown()
         turtle2.circle(2)
-        
-    #olhos()    
     
     # Criando a boca
     
@@ -210,9 +199,7 @@ while restart == 0:
         turtle2.fd(10)
         turtle2.right(180)
         turtle2.fd(20)
-        
-    #boca()    
-    
+
     # Criando a função espaço entre os traços
     
     def espaco():
@@ -232,30 +219,55 @@ while restart == 0:
             
             
         for c in esc:
-            turtle3.fd(20)
-            turtle3.penup()
-            turtle3.fd(10)
-            turtle3.pendown()
-            
             if c == ' ':
-                espaco()    
+                espaco()
+                
+            else:    
+                turtle3.fd(20)
+                turtle3.penup()
+                turtle3.fd(10)
+                turtle3.pendown()
+            
     
     tracos()
     
+    
+    # Definindo o letreiro errou
+    
+    def errou():
+        
+        turtle4.color('Red')        
+        turtle4.penup()
+        turtle4.setpos(100, 100)
+        turtle4.pendown()
+        turtle4.write('ERROU!', font = ('Arial', 30))
+        
+    # Definindo o letreiro acertou
+
+    def acertou():
+        
+        turtle4.color('Green')
+        turtle4.penup()
+        turtle4.setpos(100, 100)
+        turtle4.pendown()
+        turtle4.write('ACERTOU!', font = ('Arial', 30))
+        
     
     # Criando a função while para repetir o programa
     
     acertos = 0
     erros = 0
     
-    while erros <= 8:
+    for e in range(0,len(formatada)):              # Conta cada espaço da palavra escolhida 
+
+        if formatada[e] == ' ':              # Aumenta o placar dos acertos a cada espaço
+            acertos += 1
+    
+    while erros < 8 and restart==0:
         
         p = window.textinput("Pergunta", "Insira uma letra:")
             
         for i in range(0,len(formatada)):
-
-            if formatada[i] == ' ':
-                acertos += 1
                 
             if p == formatada[i]:
                 
@@ -263,40 +275,61 @@ while restart == 0:
                 turtle3.setx(-300+i*30)
                 turtle3.sety(-185)
                 turtle3.pendown()   
-                turtle3.write(p, font= ("Arial",25))
+                turtle3.write(esc[i], font = ("Arial",25))
                 
                 acertos += 1
+                
+                turtle4.clear()
+                acertou()
                 print(acertos)
                 
         if p not in formatada:
                 erros += 1
+                print(erros)
             
         if erros == 1 and p not in formatada:
            cabeca()
+           turtle4.clear()
+           errou()
            
         if erros == 2 and p not in formatada:
             corpo()
+            turtle4.clear()
+            errou()
             
         if erros == 3 and p not in formatada:
             perna1()
+            turtle4.clear()
+            errou()
             
         if erros == 4 and p not in formatada:
             perna2()
+            turtle4.clear()
+            errou()
             
         if erros == 5 and p not in formatada:
             braco1()
+            turtle4.clear()
+            errou()
             
         if erros == 6 and p not in formatada:
             braco2()
+            turtle4.clear()
+            errou()
             
         if erros == 7 and p not in formatada:
             olhos()
+            turtle4.clear()
+            errou()
             
         if erros == 8 and p not in formatada:
             boca()
+            turtle4.clear()
+            errou()
             restart = 1
             
-        if acertos >= len(formatada)-1:
+            
+        if acertos == len(formatada):
             restart = 1
             
     f = window.textinput("Fim", "Fim do jogo! Se quiser jogar de novo, digite s, senão, n")
@@ -304,6 +337,7 @@ while restart == 0:
         turtle.clear()
         turtle2.clear()
         turtle3.clear()
+        turtle4.clear()
         restart = 0
         
     else:
